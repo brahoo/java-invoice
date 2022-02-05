4e9b707751901cb1ba3e027fa67a1e4578caff07
Piotrek <piotr.pogorzelec@gazeta.pl>
2022-02-05 17:36:17 +0100

    7 of 13 tests working, change getNetTotal

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,8 +1,6 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,24 +9,22 @@ import pl.edu.agh.mwo.invoice.product.Product;
 public class Invoice {
     private Map<Product, Integer> products = new HashMap<>();
 
-
-
-    public void addProduct(Product product) {
-       // this.products.put(product, 1);
-        addProduct(product,1);
-    }
-
     public void addProduct(Product product, Integer quantity) {
         this.products.put(product, quantity);
     }
 
-    public BigDecimal getSubtotal() {
+    public void addProduct(Product product) {
+        addProduct(product,1);
+    }
+
+    public BigDecimal getNetTotal() {
 
         BigDecimal sum = BigDecimal.ZERO;
         for (Product product : this.products.keySet()) {
-            for (int i = 0; i < products.get(product); i++) {
-                sum = sum.add(product.getPriceWithTax());
-            }
+            Integer quantity = this.products.get(product);
+            BigDecimal quantityAsBigDecimal = BigDecimal.valueOf(quantity);
+            BigDecimal priceOfThisItem = product.getPrice().multiply(quantityAsBigDecimal);
+            sum = sum.add(priceOfThisItem);
         }
         return sum;
     }
